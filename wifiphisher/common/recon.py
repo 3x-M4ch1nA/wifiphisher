e9881de1016518cc2369a02f06a8720f241c6add
Brian Smith <brian.smith@riseup.net>
2018-07-01 10:19:31 -0400

    chore:make recon file 2/3 compatible
    Make recon module python 2/3 compatible.

--- wifiphisher/common/recon.py
+++ wifiphisher/common/recon.py
@@ -4,6 +4,14 @@ available access points
 """
 
 from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+from builtins import str
+from builtins import *
+from builtins import object
 import threading
 import time
 import logging
