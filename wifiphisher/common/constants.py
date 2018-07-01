5e3c9723e53888ea87f1f7965b70dea3a3e36b2d
Brian Smith <brian.smith@riseup.net>
2018-07-01 13:06:39 -0400

    chore:make constants file 2/3 compatible
    Make constants file python 2/3 compatible.

--- wifiphisher/common/constants.py
+++ wifiphisher/common/constants.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 # pylint: skip-file
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
 import os
 
 dir_of_executable = os.path.dirname(__file__)
@@ -21,7 +24,7 @@ EXTENSIONS_LOADPATH = "wifiphisher.extensions."
 PORT = 8080
 SSL_PORT = 443
 CHANNEL = 6
-ALL_2G_CHANNELS = range(1, 14)
+ALL_2G_CHANNELS = list(range(1, 14))
 WEBSITE = "https://wifiphisher.org"
 PUBLIC_DNS = "8.8.8.8"
 PEM = dir_of_data + 'cert/server.pem'
